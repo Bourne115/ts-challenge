30d58d024aaeebccbfbdfda4bc2e462838dea081
qile <caowenpeng66@163.com>
2023-06-23 11:11:04 +0800

    init: :tada: :rocket:

--- src/hello-world/hello-world.ts
+++
@@ -1,2 +0,0 @@
-const hi:string = 'hello world'
-const name: 'qi' | 'le' = 'qi'
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export const name:string = 'qile'
+export const name: string = 'qile'
 const button: HTMLButtonElement | null = document.querySelector('.random')
 
 interface ICatType {
@@ -9,18 +9,36 @@ interface ICatType {
 }
 
 class Cat implements ICatType {
-    id: string;
-    url: string;
-    name: string;
-    test: false
+  id: string;
+  url: string;
+  name: string;
+  test: false
 
-    constructor() {
-      this.id = ''
-      this.url = ''
-      this.name = ''
-      this.test = false
-    }
+  constructor() {
+    this.id = ''
+    this.url = ''
+    this.name = ''
+    this.test = false
+  }
 }
 
 
-document?.addEventListener<'click'>('click' , () => {})
+document?.addEventListener<'click'>('click', () => { })
+
+
+function tfunc<T>(info: T) {
+  const { name, age } = info as unknown as { name: string, age: number }
+  return `姓名：${name}；年龄：${age}`
+}
+
+function identity<T>(arg: T): T {
+  return arg
+}
+
+identity({ number: 1234 })
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key]
+}
+
+getProperty({ a: '123', b: '456' }, 'c')
